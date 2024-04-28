dd1b8c2bd0bd7a646d121f1d71bf8d149a391e09
cndcadamson <168340325+cndcadamson@users.noreply.github.com>
2024-04-28 06:44:21 -0400

    Update ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-9ddbbfdd-6fb7-4971-a670-b26a9ef6583a/"
 
 OUTPUT_DIR="logs"
 
